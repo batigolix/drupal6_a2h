fd4d53e3d1f33da4de350800ea6f6b74c9d710c2
Boris Doesborg <bdoesborg@gmail.com>
2016-05-22 21:16:40 +0200

    aliases

---
+++ drupal6_a2h.aliases.drushrc.php
@@ -0,0 +1,46 @@
+<?php
+
+$aliases['cri.val'] = array(
+  'parent' => '@val',
+  'uri' => 'cristina.val',
+  'root' => '/Users/boris/Sites/drupal6_a2h',
+  'path-aliases' => array(
+    '%files' => 'sites/cristina.val/files',
+  ),
+);
+
+$aliases['cri.a2h'] = array(
+  'parent' => '@a2h',
+  'uri' => 'www.cristinasilvan.com',
+  'root' => '/home/doesborg/public_html/drupal6',
+  'path-aliases' => array(
+    '%files' => 'sites/cristinasilvan.com/files',
+  ),
+);
+
+$aliases['silvan8.val'] = array(
+  'parent' => '@val',
+  'uri' => 'silvan8.val',
+  'root' => '/Users/boris/Sites/silvan8/docroot',
+  'path-aliases' => array(
+    '%files' => 'sites/default/files',
+  ),
+);
+
+$aliases['boe.val'] = array(
+  'parent' => '@val',
+  'uri' => 'boermans.val',
+  'root' => '/Users/boris/Sites/drupal6_a2h',
+  'path-aliases' => array(
+    '%files' => 'sites/boermans.val/files',
+  ),
+);
+
+$aliases['boe.a2h'] = array(
+  'parent' => '@a2h',
+  'uri' => 'volkstheater-venlo.nl',
+  'root' => '/home/doesborg/public_html/drupal6',
+  'path-aliases' => array(
+    '%files' => 'sites/volkstheater-venlo.nl/files',
+  ),
+);
